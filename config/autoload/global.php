dec3bf24e1deca7f9cb5e8b3d95cd63933e84cd1
Allan Rodrigues Machado <allan.rodrigues14@hotmail.com>
2022-07-01 00:26:07 -0300

    Parte - 02  Connecting our application to the database

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,6 +12,26 @@
  * file.
  */
 
+use Laminas\Db\Adapter;
+
 return [
-    // ...
+    'service_manager' => [
+        'abstract_factories' => [
+            Adapter\AdapterAbstractServiceFactory::class
+        ],
+        'factories' => [
+            Adapter\AdapterInterface::class => Adapter\AdapterServiceFactory::class,
+        ],
+        'aliases' => [
+            Adapter\Adapter::class => Adapter\AdapterInterface::class
+        ],
+    ],
+
+    'db' => [
+        'driver' => 'pdo',
+        'dsn' => 'mysql:dbname:quiz;hostname=localshot',
+        'driver_options' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
+        ]
+    ]
 ];
